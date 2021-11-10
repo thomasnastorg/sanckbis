1df8f8d249c5418fb267ce3892606f446210634e
thomasnastorg <nastorgt62@gmail.com>
2021-11-10 10:47:46 +0100

    correction of bug!!

--- src/main.cpp
+++ src/main.cpp
@@ -85,26 +85,53 @@ int main()
     sf::Sound sound_menu;
     sound_menu.setBuffer(buffer);
     sound_menu.setVolume(50.f);
-    sound_menu.play();
+    int musiqueIsPlaying = 0;
+
+    Menu menu(640, 640);
+    int choixUser=0;
+
 
     while (window.isOpen())
     {
-
+        if(choixUser == 0){
+        menu.draw(window);
+        window.display();
+        }
         sf::Event event;
         while (window.pollEvent(event))
         {
+            if(event.type == sf::Event::KeyReleased)
+            {
+                if(event.key.code == sf::Keyboard::Z)
+                {
+                    menu.haut();
+                }
+                else if(event.key.code == sf::Keyboard::S)
+                {
+                    menu.bas();
+                }
+                if(event.key.code == sf::Keyboard::Enter)
+                {
+                    if(menu.confirmer_choix() == 0) choixUser = 1;
+                    else if(menu.confirmer_choix() == 1) choixUser = 2;
+                    else if(menu.confirmer_choix() == 2) choixUser = 3;
+                }
+            }
             if (event.type == sf::Event::Closed)
 
                 window.close();
         }
-
-
-        window.draw(sprite);
-        window.clear();
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)&& move != 2) move = 0;
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& move != 3) move = 1;
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)&& move != 0 ) move = 2;
-        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)&& move != 1) move = 3;
+        if (choixUser == 1) {
+            if (musiqueIsPlaying == 0){
+                sound_menu.play();
+                musiqueIsPlaying = 1;
+            }
+            window.draw(sprite);
+            window.clear();
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)&& move != 2) move = 0;
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)&& move != 3) move = 1;
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)&& move != 0 ) move = 2;
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)&& move != 1) move = 3;
 
         if(timer.getElapsedTime().asMilliseconds()>delai)
         {
@@ -136,5 +163,6 @@ int main()
 
         window.draw(apple);
         window.display();
+        }
     }
 }
--- src/menu.cpp
+++ src/menu.cpp
@@ -4,30 +4,36 @@
 
 #include "menu.h"
 #include "SFML/Graphics.hpp"
+#include <SFML/Audio.hpp>
 
 Menu::Menu(float width, float height)
 {
-    if (!font.loadFromFile("arial.ttf"))
+
+
+    if (!font.loadFromFile("../Font/Vandalism.ttf"))
     {
         //erreur;
     }
 
     menu[0].setFont(font);
-    menu[0].setFillColor(sf::Color::Red);
+    menu[0].setFillColor(sf::Color::Green);
     menu[0].setString("Jouer");
-    menu[0].setPosition(sf::Vector2f(width/2, height/(NB_ITEM + 1) * 1));
+    //menu[0].setOrigin(width/2,height/2);
+    menu[0].setPosition(sf::Vector2f((width/2)-50, height/(NB_ITEM + 1) * 1));
     //menu[0].setCharacterSize(60);
 
     menu[1].setFont(font);
     menu[1].setFillColor(sf::Color::White);
     menu[1].setString("Options");
-    menu[1].setPosition(sf::Vector2f(width/2, height/(NB_ITEM + 1) * 2));
+    //menu[1].setOrigin(width/2,height/2);
+    menu[1].setPosition(sf::Vector2f((width/2)-50, height/(NB_ITEM + 1) * 2));
     //menu[1].setCharacterSize(60);
 
     menu[2].setFont(font);
     menu[2].setFillColor(sf::Color::White);
     menu[2].setString("Quitter");
-    menu[2].setPosition(sf::Vector2f(width/2, height/(NB_ITEM + 1) * 3));
+    //menu[2].setOrigin(width/2,height/2);
+    menu[2].setPosition(sf::Vector2f((width/2)-50, height/(NB_ITEM + 1) * 3));
     //menu[2].setCharacterSize(60);
 
     choix=0;
@@ -40,6 +46,26 @@ Menu::Menu()
 
 void Menu::draw(sf::RenderWindow &window)
 {
+    int musiqueIsPlayingMenu =0;
+    /*sf::SoundBuffer buffer;
+    buffer.loadFromFile("../song/music.wav");
+
+    sf::Sound sound_menuu;
+    sound_menuu.setBuffer(buffer);
+    sound_menuu.setVolume(50.f);*/
+
+
+    if(musiqueIsPlayingMenu == 0){
+        //sound_menuu.play();
+        musiqueIsPlayingMenu = 1;
+    }
+
+    sf::Texture texture;
+    sf::Texture backMenu;
+    backMenu.loadFromFile("../images/retro_blur.jpg");
+    sf::Sprite sprite;
+    sprite.setTexture(backMenu);
+    window.draw(sprite);
     for (int i=0; i<NB_ITEM; i++)
     {
         window.draw(menu[i]);
@@ -53,7 +79,7 @@ void Menu::bas()
 
         menu[choix].setFillColor(sf::Color::White);
         choix++;
-        menu[choix].setFillColor(sf::Color::Blue);
+        menu[choix].setFillColor(sf::Color::Green);
 
     }
 }
@@ -63,7 +89,7 @@ void Menu::haut()
     {
         menu[choix].setFillColor(sf::Color::White);
         choix--;
-        menu[choix].setFillColor(sf::Color::Blue);
+        menu[choix].setFillColor(sf::Color::Green);
     }
 }
 int Menu::confirmer_choix()
